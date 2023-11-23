786a55e7716d1651554993bf8b5330d4261ca0ee
abergeron <abergeron1905@gmail.com>
2023-11-23 11:45:27 -0500

    implement rest api

--- pluralsight-course/course-info/course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
+++ pluralsight-course/course-info/course-info-server/src/main/java/com/pluralsight/courseinfo/server/CourseServer.java
@@ -1,5 +1,8 @@
 package com.pluralsight.courseinfo.server;
 
+import java.net.URI;
+
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,10 +14,14 @@ public class CourseServer {
     private static final Logger LOG = LoggerFactory.getLogger(CourseServer.class);
     private static final String BASE_URI = "http://localhost:8080/";
 
-    CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
-    ResourceConfig config = new ResourceConfig().register(new CourseResource((courseRepository)));
-         
+    public static void main(String... args) {
+        LOG.info("Starting HTTP server");
+        CourseRepository courseRepository = CourseRepository.openCourseRepository("./courses.db");
+        ResourceConfig config = new ResourceConfig().register(new CourseResource(courseRepository));
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config); 
+    }
+
     
-    GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+   
     
 }
